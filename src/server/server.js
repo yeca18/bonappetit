dbefb092a9e732c943681e3d32dc9f982865f1d5
yeca <yeca18@gmail.com>
2018-02-14 17:30:31 -0600

    se crea una ruta con mi nombre

--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,34 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'World' })
-})
+  res.json({ hello: 'World' });
+});
 
 app.get('/users/:id', (req, res) => {
   const users = {
     14: 'Francisco',
     15: 'Jose',
-    16: 'Maria'
-  }
-  res.json({ name: users[req.params.id] })
-})
+    16: 'Maria',
+  };
+  res.json({ name: users[req.params.id] });
+});
 
 app.get('/companies', (req, res) => {
   // parametros query. Ejemplo /companies?hola=true
-  res.json({ name: req.query.name })
-})
+  res.json({ name: req.query.name });
+});
+
+app.get('/yeca/:name', (req, res) => {
+  const users = {
+    name: 'Yessica',
+  };
+  res.json({ name: 'Mi nombre es Yessica' });
+});
+
 
 app.listen(3005, () => {
-  console.log('Server running on port 3005')
-})
+  console.log('Server running on port 3005');
+});
