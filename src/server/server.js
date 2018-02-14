3b59cfad90ad5fec57f29c64b0e1eeca0b490cd2
Brayan Salas <bsalas.0694@gmail.com>
2018-02-14 17:28:04 -0600

    Se agregan cambios personales

--- src/server/server.js
+++ src/server/server.js
@@ -20,6 +20,18 @@ app.get('/companies', (req, res) => {
   res.json({ name: req.query.name })
 })
 
+app.get('/myself', (req, res) => {
+  res.json({ name: 'Brayan', username: 'bingtrav'})
+})
+
+app.get('/myselfP/:name/:username', (req, res) => {
+  res.json({ name: req.params.name, username: req.params.username})
+})
+
+app.get('/myselfQ', (req, res) => {
+  res.json({ name: req.query.name, username: req.query.username})
+})
+
 app.listen(3005, () => {
   console.log('Server running on port 3005')
 })
